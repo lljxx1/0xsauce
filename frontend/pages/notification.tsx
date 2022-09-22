414431da423dfd6f475e07948fd9613da91bc1d3
fun <lljxx1@gmail.com>
2022-09-22 20:23:47 +0800

    add

--- frontend/pages/notification.tsx
+++ frontend/pages/notification.tsx
@@ -149,7 +149,7 @@ const TokenQuery: FC = () => {
           <div>
             <h1 className="text-2xl font-bold text-gray-900">Stolen NFT Monitor</h1>
             {/* <p className="text-sm font-medium text-gray-500">Applied for <a href="#" className="text-gray-900">Front End Developer</a> on <time datetime="2020-08-25">August 25, 2020</time></p> */}
-            <a target="_blank" href="https://app.epns.io/#/channels?channel=0xEf0D8F546880d1D41e7F35c5BA06a43C7F42FF2f"><button type="button" className="mt-6 inline-flex justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-pink-500 focus:ring-offset-2">
+            <a target="_blank" rel="noreferrer" href="https://app.epns.io/#/channels?channel=0xEf0D8F546880d1D41e7F35c5BA06a43C7F42FF2f"><button type="button" className="mt-6 inline-flex justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-pink-500 focus:ring-offset-2">
                         <svg className="-ml-1 mr-2 h-5 w-5 text-gray-400" x-description="Heroicon name: mini/envelope" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
   <path d="M3 4a2 2 0 00-2 2v1.161l8.441 4.221a1.25 1.25 0 001.118 0L19 7.162V6a2 2 0 00-2-2H3z"></path>
   <path d="M19 8.839l-7.77 3.885a2.75 2.75 0 01-2.46 0L1 8.839V14a2 2 0 002 2h14a2 2 0 002-2V8.839z"></path>
